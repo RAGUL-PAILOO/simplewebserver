982bf723cdb42db39e78e8a3c8eb4f50930204f9
RAGUL-PAILOO <ragulrajasekaran313@gmail.com>
2025-10-03 11:42:50 +0530

    Success

--- web.py
+++ web.py
@@ -1,61 +1,59 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
 content = """
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>ragul</title>
-    </head>
-    <body>
-        <h1 align="center">Register Number-25008715</h1>
-        <h2 align="center">Name-Ragul</h2>
-        <h3 align="center">SEMESTER MARKS</h3>
-        <br>
-        <center>
-            <table border="3" cellspacing="5" callpading="5">
-
-        </center>
-            <tr>
-                <th>S.no</th>
-                <th>Subject Name</th>
-                <th>Marks</th>
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>Communicative English</td>
-                <td>94</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Fundamental of Data Science</td>
-                <td>95</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>fundamental of Web Application Development</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Physics for Quantum Computing</td>
-                <td>97</td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>Digital ELectronics</td>
-                <td>96</td>
-            </tr>
-    </body>
+<head>
+    <title>Top 5 Revenue Generating Software Companies</title>
+</head>
+<body>
+    <table>
+        <caption>Top 5 Revenue Generating Software Companies</caption>
+        <tr>
+            <th>s.no</th>
+            <th>companies</th>
+            <th>revenue</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Microsoft</td>
+            <td>65 billion</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Oracle</td>
+            <td>29.6 billion</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>IBM</td>
+            <td>29.1 billion</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>SAP</td>
+            <td>6.4 billion</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Symantec</td>
+            <td>5.6 billion</td>
+        </tr>
+    </table>
+</body>
 </html>
+
 """
-class myhandler(BaseHTTPRequestHandler):
+
+class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print("request received")
+        print("Request received")
         self.send_response(200)
-        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
         self.wfile.write(content.encode())
-server_address = ('',8000)
-httpd = HTTPServer(server_address,myhandler)
-print("my webserver is running...")
+
+server_address = ('', 8000)
+httpd = HTTPServer(server_address, MyHandler)
+print("My webserver is running...")
 httpd.serve_forever()
